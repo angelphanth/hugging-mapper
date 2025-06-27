92a2bd76f18deae39766610f2f92c65b036c62f9
Angel L. P <59593766+angelphanth@users.noreply.github.com>
2025-06-28 01:55:59 +0200

    :bug: missing dep for readthedocs build

--- hugger/__init__.py
+++ hugger/__init__.py
@@ -4,7 +4,7 @@ from importlib import metadata
 
 __version__ = metadata.version("hugger")
 
-from .mapper import HuggingMapper, NodeMapper
+#from .mapper import HuggingMapper, NodeMapper
 
 # The __all__ variable is a list of variables which are imported
 # when a user does "from example import *"
